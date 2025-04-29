705b3ef3fc1ede2ff7f864004bbe8031a317fbc2
Mohamad Nour Abo Jamil <moa010@edu.zealand.dk>
2025-04-29 11:08:58 +0200

    ddaw

--- ClassLibrary1/Boat.cs
+++ ClassLibrary1/Boat.cs
@@ -10,6 +10,21 @@
         public int BuildYear { get; set; }
         public bool IsAvailable { get; set; }
 
-        public List<Maintenance> MaintenanceLog { get; set; } = new();
+        public Boat(int BoatID, string name, string model, string sailNumber, string engineInfo, int builderYear, bool isAvailable)
+        {
+            Name = name;
+            Model = model;
+            SailNumber = sailNumber;
+            EngineInfo = engineInfo;
+            BuildYear = builderYear;
+            IsAvailable = isAvailable;
+
+        }
+        public override string ToString()
+        {
+            return $"{Name},{Model},{SailNumber},{EngineInfo}, {IsAvailable}";
+        }
+
+        // public List<Maintenance> MaintenanceLog { get; set; } = new();
     }
 }
--- ClassLibrary1/BoatBooking.cs
+++ ClassLibrary1/BoatBooking.cs
@@ -1,18 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualBasic;
 
 namespace ClassLibrary1
 {
-    public class BoatBooking
+    public class BoatBooking : Boat
     {
-        public int BookingId { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-        public string Destination { get; set; }
-        public Member BookedBy { get; set; }
-        public Boat Boat { get; set; }
+
+
+        public int BookingID { get; set; }
+        public int BådID { get; set; }
+        public int MedlemsID { get; set; }
+        public DateTime SejlTid { get; set; }
+        public string Mødested { get; set; }
+
+
+        public BoatBooking(int BookingID, int BådID, int MeldemsID, DateAndTime Sejltid, string mødested) { }
+
+        public void BookBåd(int bådID, int medlemsID, DateTime sejlTid, string mødested)
+        {
+            Booking nyBooking = new Booking
+            {
+                BookingID = GenerateBookingID(), // funktion til at generere ID
+                BådID = bådID,
+                MedlemsID = medlemsID,
+                SejlTid = sejlTid,
+                Mødested = mødested
+            };
+
+            // Her kan du tilføje booking til en database eller liste
+            bookings.Add(nyBooking);
+
+            Console.WriteLine("Båd er blevet booket til den ønskede tid.");
+        }
+
     }
 }
